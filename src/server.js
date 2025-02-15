35b7cb5fa2d465ee077054eca94e200f73331464
Matthieu Nzinga <matthieunzinga@gmail.com>
2025-02-15 12:03:27 +0100

    Intégration de Swagger dans le serveur

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js';
 import projectRoutes from './routes/project.routes.js';
+import setupSwagger from './swagger.js';
 
 dotenv.config();
 const app = express();
@@ -10,10 +11,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+setupSwagger(app);
+
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`📖 Documentation disponible sur http://localhost:${PORT}/api-docs`);
 });
