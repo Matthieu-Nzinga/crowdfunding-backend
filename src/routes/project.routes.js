e6248dafc12c96c9636fe366e296195c8f02d95b
Matthieu Nzinga <matthieunzinga@gmail.com>
2025-02-15 12:05:25 +0100

    Ajout des annotations Swagger pour la documentation

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,3 +1,41 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Gestion de l'authentification
+ */
+
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     summary: Inscription d'un utilisateur
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Utilisateur inscrit avec succès
+ *       400:
+ *         description: Email déjà utilisé
+ */
+
+
 import express from 'express';
 import { createProject, getAllProjects } from '../controllers/project.controller.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
